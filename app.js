4af9ae48c35a7acacfcd58a0530c36726a63ab9a
Darkwa <jnrdrkw1@gmail.com>
2022-04-18 20:52:42 +0000

    added functionality to generate link

---
+++ app.js
@@ -0,0 +1,33 @@
+const selectElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw new Error(`Cannot find the element ${selector}`);
+};
+
+const form = selectElement("form");
+const input = selectElement("input");
+const result = selectElement(".result");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const url = input.value;
+
+  shortenUrl(url);
+});
+
+async function shortenUrl(url) {
+  try {
+    const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+    const data = await res.json();
+    const newUrl = document.createElement("div");
+    newUrl.classList.add("item");
+    newUrl.innerHTML = `
+   <p> ${data.result.short_link}</p>
+   <button class="newUrl-btn" >Copy</button>
+   `;
+    result.prepend(newUrl);
+    input.value = "";
+  } catch (err) {
+    console.log(err);
+  }
+}
